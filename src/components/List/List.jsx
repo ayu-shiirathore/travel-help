b384c8cfae3266b01c8b6a529a68e6225741129d
ayu-shiirathore <ayurathore2307@gmail.com>
2024-06-05 17:41:08 +0530

    map and scroll function

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,19 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, createRef} from 'react';
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 import useStyles from './styles';
 
-const List = ( { places} ) => {
+const List = ( { places, childClicked ,isLoading} ) => {
 
     const classes= useStyles();
     const [type, setType]= useState('restaurants');
     const [rating, setRating]= useState('');
+    const [elRefs, setElRefs] = useState([]);
+
+    useEffect(() => {
+        setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
+      }, [places]);
 
 
     return (
@@ -16,6 +21,12 @@ const List = ( { places} ) => {
        <Typography variant ="h4">
         Restaurants, Hotels & Attractions around you
        </Typography>
+       {isLoading ? (
+        <div className={classes.loading}>
+        <CircularProgress size="5rem" />
+        </div>
+       ) : (
+        <>
        <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
         <Select value= {type} onChange={(e) => setType(e.target.value)}>
@@ -37,11 +48,16 @@ const List = ( { places} ) => {
 
        <Grid container spacing ={3} className ={classes.list}>
         {places?.map((place, i) => (
-            <Grid item key ={i} xs= {12}>
-            <PlaceDetails place= {place} />
+            <Grid ref={elRefs[i]} item key ={i} xs= {12}>
+            <PlaceDetails
+             place= {place}
+            selected={Number(childClicked) === i} refProp={elRefs[i]}
+             />
             </Grid>
         ))}
        </Grid>
+       </>
+       )}
 
        </div>
     );
